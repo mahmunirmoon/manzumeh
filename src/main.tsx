2cdde412ae8cc3616ff47a74d0055209e3afe77b
mahmunirmoon <DRMahMonir@proton.me>
2026-09-01 09:01:49 +0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./mobile-fixes.css";
 import App from "./App.tsx";
+import { HelpWidget } from "./components/HelpWidget";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+    <HelpWidget />
+  </React.StrictMode>,
+);
